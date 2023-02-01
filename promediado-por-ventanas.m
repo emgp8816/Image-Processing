aa08a3da4147d0d05182a56966752ebe87a86081
emgp8816 <73894676+emgp8816@users.noreply.github.com>
2023-02-01 00:46:09 -0600

    Update promediado-por-ventanas.m

--- promediado-por-ventanas.m
+++ promediado-por-ventanas.m
@@ -1,24 +1,18 @@
+clc,clear,close all
 imagen=imread('lobo.jpg');
 grises=rgb2gray(imagen);
-%imshow(grises)
-gris=grises(1:512,1:512);
-gris1=double(gris);
-imshow(gris)
-tam=size(gris);
-a=tam(1); 
-gris21=zeros(a);
+imshow(grises)
+tam=size(grises);
+gris_n=zeros(tam(1));
 for k=1:6
     base=2^k;
-    matriz=zeros(base);
-    for (i=1:base:a-base+1)
-        for (j=1:base:a-base+1)
-            mean1=mean(gris1(i:base+i-1,j:j+base-1));
+    for i=1:base:tam(1)-base+1
+        for j=1:base:tam(1)-base+1
+            mean1=mean(grises(i:base+i-1,j:j+base-1));
             mean2=mean(mean1);
-            matriz(:,:)=mean2;
-            gris21(i:base+i-1,j:j+base-1)=matriz;
+            gris_n(i:base+i-1,j:j+base-1)=mean2;
         end
     end
 figure
-gris2=uint8(gris21);
-imshow(gris2)
+imshow(gris_n/255)
 end
