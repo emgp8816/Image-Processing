6101043af5736457b2984782b003d683c22d0bbc
emgp8816 <73894676+emgp8816@users.noreply.github.com>
2023-02-01 00:58:41 -0600

    Delete prueba.m

--- prueba.m
+++
@@ -1,6 +0,0 @@
-nombre=input('Ingrese el nombre de la imagen con su extensión:','s');
-imagen=imread(nombre);
-gris=rgb2gray(imagen);
-region=logical(region);
-region = bwlabel(region,4);
-imshow(region/255)
